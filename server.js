d3eb08b5abc798d156c0cc04e1b6229c63e385fb
unknown <nuris.akbar@gmail.com>
2016-12-05 00:42:40 +0700

    add crud for todo

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express 		= require('express');
 const app     		= express();
+// Load mongoose package
 var mongoose = require('mongoose');
 const swig 			= require('swig');
 const bodyParser 	= require('body-parser');
+
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'));
+
 
 swig.setDefaults({ cache: false });
 app.engine('swig', swig.renderFile);
@@ -11,23 +16,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-// mongoose area
-
+// Connect to MongoDB and create/use database called todoAppTest
 mongoose.connect('mongodb://localhost/todoAppTest');
+// Create a schema
 var TodoSchema = new mongoose.Schema({
   name: String,
   description: String,
+});
+// Create a model based on the schema
 var Todo = mongoose.model('Todo', TodoSchema);
 
-// routing area
 app.get('/',function(req,res){
-	const data ={
-		list : Todo.find(function (err, todos) {
-		if (err) return console.error(err);
-	  	console.log(todos)
+		list : Todo.find(function (err, todos) 
+		{
+		if (err) return console.error(err)
+			else
+				res.render('index',{list:todos});
+
 	})
-	}
-	res.render('index',list);
+	
 });
 
 app.get('/create',function(req,res){
@@ -35,7 +42,6 @@ app.get('/create',function(req,res){
 });
 
 app.post('/store',function(req,res){
-  console.log(req.body);
   var todo = new Todo({name: req.body.name, description: req.body.description});
 	  todo.save(function(err){
 	  if(err)
@@ -47,6 +53,44 @@ app.post('/store',function(req,res){
 });
 
 
+app.get('/edit/:id',function(req,res){
+	const id = req.params.id;
+    const query = {
+      _id: id,
+    };
+	Todo.findOne(query,function (err, result) {
+  	if (err) return console.error(err);
+  	else
+  		res.render('edit',{todo:result})
+	})
+});
+
+app.post('/update/',function(req,res){
+	const query = {
+      _id: req.body.id,
+    };
+    const update = {name: req.body.name, description: req.body.description};
+    Todo.findOneAndUpdate(query, update,function(err,result){
+    	if(err)
+    		console.log(err);
+    	else
+    		console.log(result);
+    });
+  res.redirect('/');
+});
+
+app.delete('/delete/',function(req,res){
+	const query = {
+      _id: req.body.id,
+    };
+    Todo.findOneAndRemove(query,function(err,result){
+    	if(err)
+    		console.log(err);
+    	else
+    		console.log(result);
+    });
+  res.redirect('/');
+});
 
 
 app.listen(3000, function () {
